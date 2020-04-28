bcf5088f620ad3b32c2cb5ff2cb0e9770de1d4d4
Dima Makhnach <dimam@personal.guide>
2020-04-28 04:48:25 +0300

    Done

---
+++ components/ocCardItem/index.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="card-item">
+    <div class="card-item__list">
+      <Carousel
+        :nav="true"
+        :autoWidth="true"
+        :dots="true"
+        :items="item.previewGallery"
+      >
+        <img
+        v-for="gallery in item.previewGallery"
+        :key="gallery"
+        :src="gallery"
+        :alt="item.name"
+        width="252"
+        height="168"
+        class="card-item__image">
+      </Carousel>
+    </div>
+
+    <h3 class="card-item__title">{{ item.name }}</h3>
+
+    <p class="card-item__settings">
+      <span class="card-item__rate">{{ item.rating }}</span>
+      <span class="card-item__reviews">{{ `${item.reviews} ${numToStr(item.reviews)}` }}</span>
+      <span class="card-item__type">{{ item.type }}</span>
+    </p>
+
+    <button
+        @click="$emit('setLike', item.id)"
+        :class="{ 'card-liked': isLiked }"
+        type="button"
+        class="card-item__button-like">Поставить лайк</button>
+
+    <p class="card-item__address">{{ item.address }}</p>
+
+    <ul class="card-item__attrs">
+      <li
+        v-for="attr in item.attrs"
+        :key="attr"
+        class="card-item__attr">{{ attr }}</li>
+    </ul>
+
+    <p class="card-item__price-container">
+      <span
+        v-if="item.oldprice"
+        class="card-item__old-price">{{ item.oldprice }} ₽</span>
+      <span class="card-item__price">от {{ item.price }} ₽</span>
+      <span class="card-item__type-price">{{ item.typePrice }}</span>
+    </p>
+
+  </div>
+</template>
+
+<script src="./index.js"/>
+<style lang="stylus" src="./index.styl"/>
