3f94e609924e0cb0169bf7d1265a7426c611b6a9
dimam <dimam@personal.guide>
2020-07-01 01:42:16 +0300

    Add meta tag

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,6 +8,7 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { 'http-equiv': 'content-type', content: 'application/javascript' },
       {
         hid: 'description',
         name: 'description',
